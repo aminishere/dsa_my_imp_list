dff033d0cc2133126833d7c05a86523ebbd45328
amin <106171699+aminishere@users.noreply.github.com>
2025-12-18 00:37:34 +0530

    Implement addTwoNumbers function for linked lists

---
+++ linked_list/add_two_numbers.cpp
@@ -0,0 +1,25 @@
+//lc 2
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy= new ListNode();
+        ListNode* curr= dummy;
+        int carry =0;
+        while(l1 or l2 or carry){
+            int sum = carry;
+            if(l1){
+                sum+=l1->val;
+                l1=l1->next;
+            }
+            if(l2){
+                sum+=l2->val;
+                l2=l2->next;
+            }
+            curr->next= new ListNode(sum%10);
+            carry= sum/10;
+            curr=curr->next;
+        }
+        return dummy->next;
+    }
+};
