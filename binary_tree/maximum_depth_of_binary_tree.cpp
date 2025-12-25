435e385184ca57c2cd48e36fa04899e7ceecc65d
amin <106171699+aminishere@users.noreply.github.com>
2025-12-26 01:26:19 +0530

    Add maxDepth function for binary tree depth calculation
    Implement recursive function to find maximum depth of a binary tree.

---
+++ binary_tree/maximum_depth_of_binary_tree.cpp
@@ -0,0 +1,11 @@
+//lc 104
+//main func recursion
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(!root) return 0;
+        int l= maxDepth(root->left);
+        int r= maxDepth(root->right);
+        return 1+max(l, r);
+    }
+};
