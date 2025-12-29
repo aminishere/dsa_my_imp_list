881d02aacf8d7cb086192222872a58381b5d4b88
amin <106171699+aminishere@users.noreply.github.com>
2025-12-29 17:48:02 +0530

    Create path_sum.cpp

---
+++ binary_tree/path_sum.cpp
@@ -0,0 +1,10 @@
+//lc 112
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(!root) return false;
+        targetSum-=root->val;
+        if(!root->left and !root->right)return targetSum==0;
+        return hasPathSum(root->left, targetSum) or hasPathSum(root->right, targetSum);        
+    }
+};
