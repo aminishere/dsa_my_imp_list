e7f28ef8f4c3069b0dbd3845106a8c372da5926c
amin <106171699+aminishere@users.noreply.github.com>
2025-12-29 23:02:00 +0530

    Create LCA_of_BST.cpp

---
+++ binary_tree/LCA_of_BST.cpp
@@ -0,0 +1,17 @@
+//lc 235
+//bst property
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        while(root){
+            if(root->val>p->val and root->val>q->val){
+                root=root->left;
+            }else if(root->val<p->val and root->val<q->val ){
+                root=root->right;
+            }else{
+                return root;
+            }
+        }
+        return NULL;
+    }
+};
